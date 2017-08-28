a552a311a958061e80b72172baae3e545265deec
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-08-28 16:00:51 -0500

    ft: Add form transitions

--- orders_entry/models.py
+++ orders_entry/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class EntryOrder(models.Model):
+    order = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now=True)
+    on_tow = models.BooleanField(default = True)
+    # adviser  FK
+    # appraiser FK
+    #
+
+    class META:
+        verbose_name = 'Órden de entrada'
+        verbose_name_plural = 'Órdenes de entrada'
+
+    def __str__(self):
+        return '%s' % self.order
