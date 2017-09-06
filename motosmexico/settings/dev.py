8d227f100378ab7a7c28d40241f5fe60083610f7
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-09-06 16:19:31 -0500

    fix: Fix db_motos.sqlite3 path"

--- motosmexico/settings/dev.py
+++ motosmexico/settings/dev.py
@@ -12,6 +12,6 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db_motos.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db_motos.sqlite3'),
     }
 }
