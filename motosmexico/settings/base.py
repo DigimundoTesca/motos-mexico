b822ed8e763cab85d7eef5900c79f61d0c30c140
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-09-06 16:49:18 -0500

    Fix: Fix base_dirs paths

--- motosmexico/settings/base.py
+++ motosmexico/settings/base.py
@@ -123,16 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '/static')
 ]
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/staticfiles')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/media')
 
 LOGIN_URL = '/login/'
 
